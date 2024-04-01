d832d4bbcbbffbd3f8105fb0a01e676e40459567
HyungHoKim00 <rlagudgh1109@gmail.com>
2024-04-01 16:24:57 +0900

    refactor: snakeyaml을 사용하여 파일 읽어오기, yml 파일 계층구조 생성

--- src/main/java/chess/dao/ConnectionGenerator.java
+++ src/main/java/chess/dao/ConnectionGenerator.java
@@ -1,13 +1,16 @@
 package chess.dao;
 
+import chess.dto.resource.DbUrlDto;
+import chess.dto.resource.ResourceDto;
 import chess.exception.ConnectionException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+import org.yaml.snakeyaml.LoaderOptions;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
 
 public class ConnectionGenerator {
     public Connection getConnection() {
@@ -18,22 +21,20 @@ public class ConnectionGenerator {
         try {
             FileInputStream fis = new FileInputStream(
                     "src/main/resources/" + connectionType + ".yml");
-            Properties properties = new Properties();
-            properties.load(fis);
+            Yaml yaml = new Yaml(new Constructor(ResourceDto.class, new LoaderOptions()));
+            ResourceDto resourceDto = yaml.load(fis);
+            DbUrlDto dbUrlDto = resourceDto.getDb();
             return DriverManager.getConnection(
-                    "jdbc:mysql://" + properties.get("server") + "/"
-                            + properties.get("database") + properties.get("option"),
-                    properties.get("username") + "",
-                    properties.get("password") + ""
+                    "jdbc:mysql://" + dbUrlDto.getServer() + "/" + dbUrlDto.getDatabase()
+                            + dbUrlDto.getOption(),
+                    dbUrlDto.getUsername(),
+                    dbUrlDto.getPassword()
             );
         } catch (SQLException e) {
             System.err.println("DB 연결 오류:" + e.getMessage());
             e.printStackTrace();
         } catch (FileNotFoundException e) {
             System.err.println("파일이 존재하지 않습니다: " + e.getMessage());
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.err.println("입출력 오류:" + e.getMessage());
             e.printStackTrace();
         }
         throw new ConnectionException();
